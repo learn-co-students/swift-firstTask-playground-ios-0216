f1d0b7fb81151bb7290c2c9a38acfc1e2155136a
susanlovaglio <susan@flatironschool.com>
2016-07-09 23:06:05 -0400

    quizo donzo

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,6 +15,7 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
+var accountBalance: Int = 0
 
 
 
@@ -23,7 +24,7 @@
 ### 2. You went to your local pet store and purchased yourself a puppy. You decided to name the puppy Bella. Once you named her, that name will stick with her forever. Create a variable that stores the name of your new puppy.
 */
 // write your code here
-
+let puppysName = "Bella"
 
 
 
@@ -31,7 +32,7 @@
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
 // write your code here
-
+print(puppysName)
 
 
 
@@ -40,14 +41,15 @@
 */
 // write your code here
 
-
+print("I just got a new puppy named \(puppysName) and she is awesome!")
 
 
 /*: question5
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
 // write your code here
-
+accountBalance = 0
+print("I have $\(accountBalance) in my bank account")
 
 
 
@@ -55,6 +57,7 @@
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
+print("I now have \(accountBalance)")
 
 
 
@@ -63,6 +66,7 @@
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
+//puppysName = "somethingelse" - constants can't be changed, once initialized with a value it will be there forever.
 
 
 /*:
